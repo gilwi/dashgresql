3e0734e7e72fd6f1a05cf82d515eb16c47047b27
gilwi <louisdelfieux@gmail.com>
2026-03-26 22:03:05 +0100

    add basic user logic

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from config import Config
 
 app = Flask(__name__)
 # config.py
 app.config.from_object(Config)
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = "login"
+from app import models, routes
