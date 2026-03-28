08605fdc5878ad5c7a2bb18482dbe231f186505a
gilwi <louisdelfieux@gmail.com>
2026-03-28 10:35:01 +0100

    initial commit

--- dashgresql-front/src/main.js
+++ dashgresql-front/src/main.js
@@ -2,9 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router' // Import the router we created
+import router from './router'
 
 const app = createApp(App)
 
-app.use(router) // Tell Vue to use the router
+app.use(router)
+
 app.mount('#app')
