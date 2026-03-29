5439ce626db467094f5aabbcf4a79d2369ac21f2
gilwi <louisdelfieux@gmail.com>
2026-03-29 07:59:07 +0200

    remove useless blueprints

--- dashgresql-back/app/__init__.py
+++ dashgresql-back/app/__init__.py
@@ -32,15 +32,6 @@ def create_app(config_class=Config):
     CORS(app, origins=["http://localhost:5173"])
     jwt.init_app(app)
 
-    from app.errors import bp as errors_bp
-
-    app.register_blueprint(errors_bp)
-
-    from app.main import bp as main_bp
-
-    app.register_blueprint(main_bp)
-
-    # Register your existing blueprints...
     from .auth import auth_bp
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
